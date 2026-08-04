6495ff76965844351f3da36f107c0143008d21f8
rajpatil005 <rajvardhan.patil525@gmail.com>
2026-08-04 12:42:21 +0530

    add error handling in agent_service

--- ai-service/app/services/agent_service.py
+++ ai-service/app/services/agent_service.py
@@ -3,24 +3,32 @@ from app.agents.bus_agent import agent
 
 async def generateAgentAnswer(question: str):
 
-    response = await agent.ainvoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": question
-                }
-            ]
-        }
-    )
+    try:
 
-    final_message = response["messages"][-1]
-
-    if isinstance(final_message.content, list):
-        return "".join(
-            block["text"]
-            for block in final_message.content
-            if block["type"] == "text"
+        response = await agent.ainvoke(
+            {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": question
+                    }
+                ]
+            }
         )
 
-    return final_message.content
+        final_message = response["messages"][-1]
+
+        if isinstance(final_message.content, list):
+            return "".join(
+                block["text"]
+                for block in final_message.content
+                if block["type"] == "text"
+            )
+
+        return final_message.content
+
+
+    except Exception as e:
+        print("Agent Error:", e)
+
+        return "Sorry, I am unable to process your request right now."
